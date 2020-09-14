1d3a02546878d250ed3f3b87ac379c25e7afcab6
Kamil Samaj <kamil.samaj@accolade.com>
2020-09-14 12:09:55 +0200

    add list subcommand, improve usage

--- bin/npmx.sh
+++ bin/npmx.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 
 NPMX_BASE_DIR="$HOME/.npmx"
+NPMX_SHORT_COMMAND=$(basename "$0")
 
 function check_npmx_base_dir() {
     if [[ ! -d "$NPMX_BASE_DIR" ]]; then
@@ -18,16 +19,25 @@ function check_npmx_is_in_path() {
     fi
 }
 
+# check if 'npm' command is present
+function check_npm_is_installed() {
+    if ! command -v npm &> /dev/null; then
+        echo "'npm' could not be found"
+        exit 1
+    fi
+}
+
 # print help message
 function usage() {
 cat << EOF
-    Usage: $0 {install, uninstall, update, help} <npm_package_name>
+    Usage: $NPMX_SHORT_COMMAND {list, install, uninstall, update, help} [<npm_package_name>]
 
     Subcommands:
-        install             Install a new NPM package to it's own subdirectory
-        update              Update an existing NPMX installation
-        uninstall           Uninstall an existing NPMX installation
-        help                Print this help message
+        list                                List installed NPMs
+        install <npm_package_name>          Install a new NPM package to it's own subdirectory
+        update <npm_package_name>           Update an existing NPMX installation
+        uninstall <npm_package_name>        Uninstall an existing NPMX installation
+        help                                Print this help message
 EOF
 }
 
@@ -53,7 +63,7 @@ function create_symlinks() {
     fi
 
     for bin_file in "$src_bin_dir"/*; do
-        short_bin_file="$(basename $bin_file)"
+        short_bin_file=$(basename "$bin_file")
         if [[ ! -f "$dst_bin_dir/$short_bin_file" ]]; then
             echo "Found commnand $short_bin_file, creating a symlink to it"
             ln -s \
@@ -63,6 +73,17 @@ function create_symlinks() {
             echo "WARNING: File $dst_bin_dir/$short_bin_file already exists, not creating a symlink"
         fi
     done
+}
+
+# print a missing argument error and usage
+# usage: echo_missing_arg subcommand
+function echo_missing_arg() {
+    local subcommnand
+    subcommnand="$1"
+    echo "\"$NPMX_SHORT_COMMAND $subcommnand <npm_pkg_name>\" requires a package name" >&2
+    echo
+    usage
+    exit 1
 }
 
 # unlink previous symbolic links
@@ -87,7 +108,7 @@ function unlink_symlinks() {
     fi
 
     for bin_file in "$src_bin_dir"/*; do
-        short_bin_file="$(basename $bin_file)"
+        short_bin_file=$(basename "$bin_file")
         if [[ -f "$dst_bin_dir/$short_bin_file" ]]; then
             unlink "$dst_bin_dir/$short_bin_file"
         else
@@ -105,13 +126,13 @@ function install() {
     mkdir -p "$NPMX_BASE_DIR/npms/$npmx_pkg_name"
 
     if [[ -d "$NPMX_BASE_DIR/npms/$npmx_pkg_name/node_modules/.bin" ]]; then
-        echo "ERROR: $npmx_pkg_name already installed, run '$0 update ...' instead. Exiting ..."
+        echo "ERROR: $npmx_pkg_name already installed, run '$NPMX_SHORT_COMMAND update ...' instead. Exiting ..."
         exit 1
     fi
     # run in a sub-shell to return back
     ( \
         cd "$NPMX_BASE_DIR/npms/$npmx_pkg_name"; \
-        npm install "$npmx_pkg_name"; \
+        npm install "$npmx_pkg_name" --loglevel=error; \
         create_symlinks \
             "$NPMX_BASE_DIR/npms/$npmx_pkg_name/node_modules/.bin" \
             "$NPMX_BASE_DIR/bin"
@@ -125,7 +146,7 @@ function update() {
     npmx_pkg_name="$1"
 
     if [[ ! -d "$NPMX_BASE_DIR/npms/$npmx_pkg_name/node_modules/.bin" ]]; then
-        echo "ERROR: $npmx_pkg_name NOT installed, run '$0 install ...' instead. Exiting ..."
+        echo "ERROR: $npmx_pkg_name NOT installed, run '$NPMX_SHORT_COMMAND install ...' instead. Exiting ..."
         exit 1
     fi
 
@@ -138,7 +159,7 @@ function update() {
     ( \
         cd "$NPMX_BASE_DIR/npms/$npmx_pkg_name"; \
         echo "Updating $npmx_pkg_name"; \
-        npm update "$npmx_pkg_name"; \
+        npm update "$npmx_pkg_name" --loglevel=error; \
         create_symlinks \
             "$NPMX_BASE_DIR/npms/$npmx_pkg_name/node_modules/.bin" \
             "$NPMX_BASE_DIR/bin"
@@ -165,37 +186,65 @@ function uninstall() {
     ( \
         cd "$NPMX_BASE_DIR/npms/$npmx_pkg_name"; \
         echo "Updating $npmx_pkg_name"; \
-        npm uninstall "$npmx_pkg_name"; \
+        npm uninstall "$npmx_pkg_name" --loglevel=error; \
         echo "Removing $NPMX_BASE_DIR/npms/$npmx_pkg_name directory"; \
         rm -rf "$NPMX_BASE_DIR/npms/$npmx_pkg_name"
     )
 }
 
+# list installed npmx
+function list() {
+    local npm_dir
+
+    for npm_dir in "$NPMX_BASE_DIR/npms/"*; do
+        ( \
+            cd "$npm_dir"; \
+            npm list "$(basename "$npm_dir")"; \
+        )
+    done
+}
 ### MAIN CODE
 # basic argument check
-if [[ $# -ne 2 ]]; then
+if [[ $# -eq 0 ]]; then
     usage
     exit 1
 fi
 
-NPMX_COMMAND="$1"
-NPMX_PACKAGE_NAME="$2"
-
 # do environment setup checks
+check_npm_is_installed
 check_npmx_base_dir
 check_npmx_is_in_path
 
+NPMX_COMMAND="$1"
 # run command handler
-if [[ "$NPMX_COMMAND" = "install" ]]; then
-    install "$NPMX_PACKAGE_NAME"
-elif [[ "$NPMX_COMMAND" = "update" ]]; then
-    update "$NPMX_PACKAGE_NAME"
-elif [[ "$NPMX_COMMAND" = "uninstall" ]]; then
-    uninstall "$NPMX_PACKAGE_NAME"
-elif [[ "NPMX_COMMAND" = "help" ]] || [[ "NPMX_COMMAND" = "--help" ]]; then
-    usage
-else
-    echo "Command $1 not recognized" >&2
-    usage
-    exit 1
-fi
+case "$NPMX_COMMAND" in
+    list)
+        list
+        ;;
+    install)
+        NPMX_PACKAGE_NAME="$2"
+        [[ -z "$NPMX_PACKAGE_NAME" ]] && echo_missing_arg "$NPMX_COMMAND"
+        install "$NPMX_PACKAGE_NAME"
+        ;;
+    uninstall)
+        NPMX_PACKAGE_NAME="$2"
+        [[ -z "$NPMX_PACKAGE_NAME" ]] && echo_missing_arg "$NPMX_COMMAND"
+        uninstall "$NPMX_PACKAGE_NAME"
+        ;;
+    update)
+        NPMX_PACKAGE_NAME="$2"
+        [[ -z "$NPMX_PACKAGE_NAME" ]] && echo_missing_arg "$NPMX_COMMAND"
+        update "$NPMX_PACKAGE_NAME"
+        ;;
+    help)
+        usage
+        ;;
+    --help)
+        usage
+        ;;
+    *)
+        echo "Command $1 not recognized" >&2
+        usage
+        exit 1
+        ;;
+esac
